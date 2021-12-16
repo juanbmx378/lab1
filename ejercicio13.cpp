419c2d66b7766ba6ff0266c8a117e4800f744142
juanbmx378 <95142429+juanbmx378@users.noreply.github.com>
2021-12-16 11:23:11 -0500

    intento 2

---
+++ ejercicio13.cpp
@@ -0,0 +1,19 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main(){
+    //divisores de un numero
+    int n,cont=0;
+    cout<<"digite un numero :"<<endl; cin>>n;
+    cout<<"sus duvisores son :"<<endl;
+    for (int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<" "<<endl;
+            cont++;
+        }
+    }
+
+    return 0;
+}
+*/
