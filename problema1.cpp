942dd032bfd60198ab40d4505c555d0a4bd817be
juanbmx378 <95142429+juanbmx378@users.noreply.github.com>
2021-12-16 17:07:20 -0500

    x2

--- problema1.cpp
+++ problema1.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
 int main()
 {
+    char letra;
+    cout<<"ingrese una letra"<<endl; cin>>letra;
 
+    switch (letra) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+    case 'u': cout<<"Es una vocal"<<endl; ;break;
+    default: cout<<"es una consonante :"<<endl; break;
+
+
+
+    }
 
 
 return 0;
 }
-
+*/
